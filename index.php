829c6983869100124006d000b1d3c4cc7da8d15a
AlbertoBiga <alberto.biga10@gmail.com>
2022-05-10 14:05:14 +0200

    creazione classe

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    //creazione classe
+    class Movie {
+        public $titolo;
+        public $regista;
+        public $attorePrincipale;
+        public $annoProduzione;
+        public $costo;
+    }
+
+?>
