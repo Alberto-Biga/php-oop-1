3bf58f755a8029ea9370df7cc1625f9ddf7d8e31
AlbertoBiga <alberto.biga10@gmail.com>
2022-05-10 14:09:40 +0200

    imposto costruttore

--- index.php
+++ index.php
@@ -7,6 +7,15 @@
         public $attorePrincipale;
         public $annoProduzione;
         public $costo;
+
+        //imposto una funzione "costruttore"
+        public function __construct($_titolo, $_regista, $_attorePrincipale, $_annoProduzione, $_costo){
+            $this -> titolo = $_titolo;
+            $this -> regista = $_regista;
+            $this -> attorePrincipale = $_attorePrincipale;
+            $this -> annoProduzione = $_annoProduzione;
+            $this -> costo = $_costo;            
+        }
     }
 
 ?>
